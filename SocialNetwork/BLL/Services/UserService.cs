6c58c3d92a51b457da26d2b211495e610a3bd621
Halkman <halk-man@mail.ru>
2022-04-08 20:43:41 +0500

    допилил регистрацию

--- SocialNetwork/BLL/Services/UserService.cs
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -55,6 +55,8 @@ namespace SocialNetwork.BLL.Services
 
         }
 
+        
+        
         public User Authenticate(UserAuthenticationData userAuthenticationData)
         {
             var findUserEntity = userRepository.FindByEmail(userAuthenticationData.Email);
@@ -67,6 +69,8 @@ namespace SocialNetwork.BLL.Services
             }
             return ConstructUserModel(findUserEntity);
         }
+       
+        
         public User FindByEmail(string email)
         {
             var findUserEntity = userRepository.FindByEmail(email);
@@ -74,6 +78,8 @@ namespace SocialNetwork.BLL.Services
 
             return ConstructUserModel(findUserEntity); 
         }
+       
+        
         public void Update(User user)
         {
             var updateUserEntity = new UserEntity()
@@ -104,45 +110,5 @@ namespace SocialNetwork.BLL.Services
                 );
         }
     }
-    /* public class UserService
-     {
-         IUserRepository _userRepository;
-         public UserService()
-         {
-             _userRepository = new UserRepository();
-         }
-         public void Register(UserRegistrationData userRegistrationData)
-         {
-             if (String.IsNullOrEmpty(userRegistrationData.FirstName))
-                 throw new ArgumentNullException();
-
-             if (String.IsNullOrEmpty(userRegistrationData.LastName))
-                 throw new ArgumentNullException();
-
-             if (String.IsNullOrEmpty(userRegistrationData.Password))
-                 throw new ArgumentNullException();
-
-             if (String.IsNullOrEmpty(userRegistrationData.Email))
-                 throw new ArgumentNullException();
-
-             if (userRegistrationData.Password.Length < 8)
-                 throw new ArgumentNullException();
-
-             if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
-                 throw new ArgumentNullException("error");
-
-             if (_userRepository.FindByEmail(userRegistrationData.Email) != null)
-                 throw new ArgumentNullException();
-
-             var userEntity = new UserEntity()
-             {
-                 firstname = userRegistrationData.FirstName,
-                 lastname = userRegistrationData.LastName,
-                 password = userRegistrationData.Password,
-                 email = userRegistrationData.Email
-             };
-             if (this._userRepository.Create(userEntity) == 0)
-                 throw new Exception("что то не записалось!!!");
-         }
-     }*/
+   
 }
